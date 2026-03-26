212bc12471ed2f7c109385981ed238691dff7c9b
Deiver Rovira <deiver2005@gmail.com>
2026-03-26 17:12:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       // Cloudinary — imágenes propias del conjunto (producción)
